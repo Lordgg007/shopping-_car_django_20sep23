e155007770e0c22ffcf9b8135d57874f2ed8472d
Lordgg007 <supergaudy1@gmail.com>
2023-09-29 19:41:54 -0400

    feat: add terms and condition/privacy and about pages ; also fix about

--- core/views.py
+++ core/views.py
@@ -16,6 +16,16 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html')
 
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def terms(request):
+    return render(request, 'core/terms.html')
+
+def privacy(request):
+    return render(request, 'core/privacy.html')
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
